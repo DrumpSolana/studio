31d773d404711b3240d5b1171d78af16d13808b8
Husnain Ali <husnain%poloventures.io@gtempaccount.com>
2025-07-15 21:58:43 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Header>
    
    with the

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -28,7 +28,7 @@ export default function Header() {
     return (
         <header className="absolute top-0 left-0 right-0 z-50 bg-transparent">
             <div className="container mx-auto px-4 sm:px-6 lg:px-8">
-                <div className="flex items-center justify-between h-24 border-b border-white">
+                <div className="flex items-center justify-between h-24 border-b-2 border-white">
                     <DrumpLogo />
                     <div className="hidden md:flex items-center space-x-8">
                         <nav className="flex items-center space-x-8">
