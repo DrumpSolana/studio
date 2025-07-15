cdcb8e6f3a2619b2fd161b50927137e75b9d9912
Husnain Ali <husnain%poloventures.io@gtempaccount.com>
2025-07-15 21:52:14 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <div class="font-p

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -1,14 +1,6 @@
 import Image from 'next/image';
 import { Button } from '@/components/ui/button';
 
-const SolanaLogo = () => (
-    <svg width="24" height="24" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg" className="inline-block h-6 w-auto">
-    <path fillRule="evenodd" clipRule="evenodd" d="M21.7827 5.187C21.1444 4.562 20.25 4.75 19.825 5.562L10.2327 22.187C9.80772 23 10.0001 23.875 10.6327 24.5C11.2711 25.125 12.1654 24.938 12.5904 24.125L22.1827 7.5C22.6077 6.688 22.4211 5.812 21.7827 5.187Z" fill="white"/>
-    <path fillRule="evenodd" clipRule="evenodd" d="M10.2173 5.187C9.57894 4.562 8.68465 4.75 8.25965 5.562L-1.31296 22.187C-1.73796 23 -1.54561 23.875 -0.912963 24.5C-0.274633 25.125 0.619653 24.938 1.04465 24.125L10.637 7.5C11.062 6.688 10.8556 5.812 10.2173 5.187Z" fill="white"/>
-    <path fillRule="evenodd" clipRule="evenodd" d="M31.363 5.187C30.7246 4.562 29.8303 4.75 29.4053 5.562L19.813 22.187C19.388 23 19.5803 23.875 20.213 24.5C20.8513 25.125 21.7456 24.938 22.1706 24.125L31.763 7.5C32.188 6.688 31.9956 5.812 31.363 5.187Z" fill="white"/>
-    </svg>
-)
-
 export default function HeroSection() {
     return (
         <section className="relative min-h-screen flex items-center bg-background overflow-hidden">
@@ -17,7 +9,8 @@ export default function HeroSection() {
                 <div className="grid md:grid-cols-2 gap-8 items-center pt-24">
                     <div className="space-y-8 text-left">
                         <div className="font-poppins inline-flex items-center gap-2 border border-border rounded-lg px-3 py-1.5 text-sm text-white">
-                           Drump - First Snack on <SolanaLogo /> <span className="font-bold">SOLANA</span>
+                           Drump - First Snack on 
+                           <Image src="https://res.cloudinary.com/dwimflmjr/image/upload/v1752615308/Solana_lsczky.png" alt="Solana" width={100} height={20} />
                         </div>
                         <h1 className="text-6xl md:text-7xl font-bold font-headline text-white leading-none tracking-normal uppercase">
                             Snack, Secure, Swap
