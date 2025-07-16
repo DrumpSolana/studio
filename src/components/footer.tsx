f0aa3e3f26dfb59e45081993936c4a7c0884af57
Husnain Ali <husnain%poloventures.io@gtempaccount.com>
2025-07-16 05:05:55 +0000

    last section has solid black line on top and bottom as well

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -34,7 +34,7 @@ const legalLinks = [
 
 export default function Footer() {
     return (
-        <footer className="bg-secondary text-black font-solway">
+        <footer className="bg-secondary text-black font-solway border-t-4 border-b-4 border-black">
             <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-16 text-center">
                 <div className="flex flex-col items-center gap-6">
                     <DrumpLogo />
