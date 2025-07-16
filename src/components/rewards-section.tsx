a42ef0307dd55830a325c3b1bee455a808f75f7a
Husnain Ali <husnain%poloventures.io@gtempaccount.com>
2025-07-16 05:13:32 +0000

    we need more space after 02 and 03 section and a solid black line like s

--- src/components/rewards-section.tsx
+++ src/components/rewards-section.tsx
@@ -19,11 +19,11 @@ const rewards = [
 
 export default function RewardsSection() {
     return (
-        <section id="rewards" className="py-10 bg-primary">
+        <section id="rewards" className="pt-10 pb-20 bg-primary border-b-4 border-black">
             <div className="container mx-auto px-8 sm:px-12 lg:px-16">
                 <div className="grid md:grid-cols-2 gap-8">
                     {rewards.map((reward) => (
-                        <div key={reward.number} className="bg-secondary border-2 border-black rounded-lg p-6 shadow-[8px_8px_0px_0px_rgba(0,0,0,1)] space-y-4 flex flex-col h-[32rem]">
+                        <div key={reward.number} className="bg-secondary border-2 border-black rounded-lg p-6 shadow-[8px_8px_0px_0px_rgba(0,0,0,1)] space-y-4 flex flex-col h-[20rem]">
                             <div className="text-5xl font-bold font-headline text-black">{reward.number}</div>
                             <h3 className="text-4xl font-bold font-headline text-black leading-tight">
                                 {reward.title}
@@ -32,7 +32,7 @@ export default function RewardsSection() {
                             <p className="text-lg text-black/80 font-solway flex-grow">
                                 {reward.description}
                             </p>
-                            <div className="relative w-full h-[18rem] -mb-4">
+                            <div className="relative w-full h-[10rem] -mb-4">
                                 <Image
                                     src={reward.image}
                                     alt={reward.title}
