f9b67cae2f3a23d41ca7ec05d6ab16966166255c
Drump Punch2Earn <drumppunchearn@gmail.com>
2025-07-18 01:50:45 +0000

    Looks like the socials meta data is not showing up. why would that be th

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import { Toaster } from "@/components/ui/toaster";
 import FallingCheeseBalls from '@/components/falling-cheese-balls';
 import { AnimationProvider } from '@/contexts/AnimationContext';
 
-const siteUrl = 'https://drump.com';
+const siteUrl = 'https://drump.app';
 const imageUrl = 'https://res.cloudinary.com/dwimflmjr/image/upload/v1752640504/Drump_Cheese_Ball_4_daedqc.png';
 const siteDescription = 'First Snack on Solana';
 
@@ -46,7 +46,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <head>
-        <link rel="icon" href="https://res.cloudinary.com/dwimflmjr/image/upload/v1752640504/Drump_Cheese_Ball_4_daedqc.png" sizes="any" />
+        <link rel="icon" href={imageUrl} sizes="any" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Anton&family=Inter:wght@400;500;600;700&family=Poppins:wght@400;500;600;700;800;900&family=Solway:wght@400;700&display=swap" rel="stylesheet" />
