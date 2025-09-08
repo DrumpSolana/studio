a4c98541be1e8cd024dddafa0b1ac5fb0a313d0d
Husnain Ali <husnain%poloventures.io@gtempaccount.com>
2025-09-08 16:15:09 +0000

    i am setting up google tag manager so i have the code that needs to go i

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,7 @@ const socialImageUrl = 'https://res.cloudinary.com/dwimflmjr/image/upload/v17528
 const faviconUrl = 'https://res.cloudinary.com/dwimflmjr/image/upload/v1752640504/Drump_Cheese_Ball_4_daedqc.png';
 const siteDescription = 'First Snack on Solana';
 const GA_MEASUREMENT_ID = 'G-MT3LG8V1N2';
+const GTM_ID = 'GTM-XXXXXX'; // <-- IMPORTANT: Replace with your GTM ID
 
 export const metadata: Metadata = {
   title: 'Drump',
@@ -55,6 +56,19 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Anton&family=Inter:wght@400;500;600;700&family=Poppins:wght@400;500;600;700;800;900&family=Solway:wght@400;700&display=swap" rel="stylesheet" />
+        
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','${GTM_ID}');
+          `}
+        </Script>
+        {/* End Google Tag Manager */}
+
         <Script
           src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
           strategy="afterInteractive"
@@ -69,6 +83,17 @@ export default function RootLayout({
         </Script>
       </head>
       <body className="font-body antialiased relative">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         <AnimationProvider>
            <React.Suspense>
               <GtagProvider>
